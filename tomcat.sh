35957ae24ab43fd520afff51ae84ee5ca1597f0a
HuangDelong <josephhuanglong@gmail.com>
2018-05-06 16:53:56 +0800

    Create tomcat.sh
    tomcat start|stop|status

---
+++ tomcat.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+usage() {
+   echo  " Usage: $0 [start|stop|status]"
+}
+
+start_tomcat() {
+/usr/local/tomcat/bin/startup.sh
+}
+
+stop_tomcat() {
+
+/usr/local/tomcat/bin/shutdown.sh
+sleep 5;
+
+TPID=$(ps aux |grep java |grep tomcat |grep -v 'grep' | awk '{print $2}')
+    if [ -z $TPID ];then
+	echo "tomcat stop"
+    else
+	kill -9 $TSTAT
+        sleep 5;
+        echo "tomcat stop"
+    fi
+}
+
+status_tomcat() {
+ps aux |grep java |grep tomcat |grep -v 'grep'
+}
+
+main() {
+case "$1" in
+  start)
+     start_tomcat;;
+  stop)
+     stop_tomcat;;
+  status)
+     status_tomcat;;
+    *)
+     usage;;
+esac
+}
+main $1;
