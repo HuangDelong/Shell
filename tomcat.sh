c139099fe11ffd6eb6ae4b998a8f6e9f7f46a7e1
HuangDelong <josephhuanglong@gmail.com>
2018-05-06 17:04:40 +0800

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -7,23 +7,26 @@ usage() {
 start_tomcat() {
 rm -rf /usr/local/tomcat/temp/*
 rm -rf /usr/local/tomcat/work/*
-/usr/local/tomcat/bin/startup.sh
+/usr/local/tomcat/bin/startup.sh > /dev/null 2>&1
+        echo "##############################################"
+        echo "tomcat started"
+
 }
 
 stop_tomcat() {
 
-/usr/local/tomcat/bin/shutdown.sh
+/usr/local/tomcat/bin/shutdown.sh > /dev/null 2>&1
 sleep 5;
 
 TPID=$(ps aux |grep java |grep tomcat |grep -v 'grep' | awk '{print $2}')
     if [ -z $TPID ];then
         echo "##############################################"
-	echo "tomcat stop"
+	echo "tomcat stoped"
     else
 	kill -9 $TSTAT
         sleep 5;
 	echo "##############################################"
-        echo "tomcat stop"
+        echo "tomcat stoped"
     fi
 }
 
